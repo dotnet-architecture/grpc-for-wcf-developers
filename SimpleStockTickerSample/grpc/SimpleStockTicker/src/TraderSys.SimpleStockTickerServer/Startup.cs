fe0a7de0d53cc006fdcbb5d00eea244e036d0be1
Vaishali Raval <Vaishali.Raval@neudesic.com>
2021-10-25 18:02:34 +0530

    fixed the indentations

--- SimpleStockTickerSample/grpc/SimpleStockTicker/src/TraderSys.SimpleStockTickerServer/Startup.cs
+++ SimpleStockTickerSample/grpc/SimpleStockTicker/src/TraderSys.SimpleStockTickerServer/Startup.cs
@@ -1,34 +1,34 @@
 ﻿
 namespace TraderSys.SimpleStockTickerServer;
 
-    public class Startup
+public class Startup
+{
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddGrpc();
-            services.AddSingleton<IStockPriceSubscriberFactory, StockPriceSubscriberFactory>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<StockTickerService>();
-
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                });
-            });
-        }
+        services.AddGrpc();
+        services.AddSingleton<IStockPriceSubscriberFactory, StockPriceSubscriberFactory>();
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGrpcService<StockTickerService>();
+
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+            });
+        });
+    }
+}
