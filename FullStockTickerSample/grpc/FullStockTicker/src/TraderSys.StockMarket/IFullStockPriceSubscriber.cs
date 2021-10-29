7d54a0eb65db4dcd6595cc05c5486fe774fb5af4
Vaishali Raval <Vaishali.Raval@neudesic.com>
2021-10-30 00:14:05 +0530

    Included global using in stock market project

--- FullStockTickerSample/grpc/FullStockTicker/src/TraderSys.StockMarket/IFullStockPriceSubscriber.cs
+++ FullStockTickerSample/grpc/FullStockTicker/src/TraderSys.StockMarket/IFullStockPriceSubscriber.cs
@@ -1,4 +1,3 @@
-using System;
 
 namespace TraderSys.StockMarket;
 public interface IFullStockPriceSubscriber : IDisposable
--- FullStockTickerSample/grpc/FullStockTicker/src/TraderSys.StockMarket/StockPriceUpdateEventArgs.cs
+++ FullStockTickerSample/grpc/FullStockTicker/src/TraderSys.StockMarket/StockPriceUpdateEventArgs.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 namespace TraderSys.StockMarket;
 public class StockPriceUpdateEventArgs : EventArgs
 {
