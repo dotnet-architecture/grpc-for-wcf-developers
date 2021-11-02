6b5720e3823825d82d6173d1ae5453cbb8cd33c2
Vaishali Raval <Vaishali.Raval@neudesic.com>
2021-11-02 11:27:06 +0530

    Included file scoped namespace for TraderSys.PortfolioData

--- PortfoliosSample/grpc/TraderSysAuth/src/TraderSys.PortfolioData/Models/Portfolio.cs
+++ PortfoliosSample/grpc/TraderSysAuth/src/TraderSys.PortfolioData/Models/Portfolio.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
 
-namespace TraderSys.PortfolioData.Models
+namespace TraderSys.PortfolioData.Models;
+public class Portfolio
 {
-    public class Portfolio
-    {
-        public int Id { get; set; }
-        public Guid TraderId { get; set; }
-        public List<PortfolioItem> Items { get; set; }
-    }
+    public int Id { get; set; }
+    public Guid TraderId { get; set; }
+    public List<PortfolioItem> Items { get; set; }
 }
