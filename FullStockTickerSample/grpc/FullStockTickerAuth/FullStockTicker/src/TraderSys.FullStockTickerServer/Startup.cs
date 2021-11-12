d676c346f5fed7b4a595c117d7df8114ea3c8a98
Vaishali Raval <Vaishali.Raval@neudesic.com>
2021-11-12 13:40:02 +0530

    Included file scoped namespace for FullStockTickerServer

--- FullStockTickerSample/grpc/FullStockTickerAuth/FullStockTicker/src/TraderSys.FullStockTickerServer/Startup.cs
+++ FullStockTickerSample/grpc/FullStockTickerAuth/FullStockTicker/src/TraderSys.FullStockTickerServer/Startup.cs
@@ -14,51 +14,49 @@ using Microsoft.VisualBasic;
 using TraderSys.FullStockTickerServer.Services;
 using TraderSys.StockMarket;
 
-namespace TraderSys.FullStockTickerServer
+namespace TraderSys.FullStockTickerServer;
+public class Startup
 {
-    public class Startup
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddGrpc();
-            services.AddSingleton<IFullStockPriceSubscriberFactory, FullStockPriceSubscriberFactory>();
-            services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
-                .AddCertificate(options =>
-                {
-                    options.AllowedCertificateTypes = CertificateTypes.Chained;
-                    options.RevocationMode = X509RevocationMode.NoCheck;
-
-                    options.Events = new CertificateAuthenticationEvents
-                    {
-                        OnCertificateValidated = DevelopmentModeCertificateHelper.Validate
-                    };
-                });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+        services.AddGrpc();
+        services.AddSingleton<IFullStockPriceSubscriberFactory, FullStockPriceSubscriberFactory>();
+        services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
+            .AddCertificate(options =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                options.AllowedCertificateTypes = CertificateTypes.Chained;
+                options.RevocationMode = X509RevocationMode.NoCheck;
 
-            app.UseRouting();
-
-            app.UseCertificateForwarding();
-            app.UseAuthentication();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<FullStockTickerService>();
-
-                endpoints.MapGet("/", async context =>
+                options.Events = new CertificateAuthenticationEvents
                 {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                });
+                    OnCertificateValidated = DevelopmentModeCertificateHelper.Validate
+                };
             });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseRouting();
+
+        app.UseCertificateForwarding();
+        app.UseAuthentication();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGrpcService<FullStockTickerService>();
+
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+            });
+        });
     }
 }
