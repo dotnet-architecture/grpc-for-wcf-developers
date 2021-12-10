d2f4b0bea55852528a5bb0a46e42e5d82fe7e5a7
Vaishali Raval <Vaishali.Raval@neudesic.com>
2021-12-10 18:19:15 +0530

    Deleted Startup and updated program.cs for FullStockTickerAuth

--- FullStockTickerSample/grpc/FullStockTickerAuth/FullStockTicker/src/TraderSys.FullStockTickerServer/Program.cs
+++ FullStockTickerSample/grpc/FullStockTickerAuth/FullStockTicker/src/TraderSys.FullStockTickerServer/Program.cs
@@ -1,26 +1,40 @@
+using TraderSys.FullStockTickerServer;
 
-namespace TraderSys.FullStockTickerServer;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+var builder = WebApplication.CreateBuilder(args);
 
-    // Additional configuration is required to successfully run gRPC on macOS.
-    // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+
+// Register the repository class as a scoped service (instance per request)
+builder.Services.AddSingleton<IFullStockPriceSubscriberFactory, FullStockPriceSubscriberFactory>();
+builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
+            .AddCertificate(options =>
             {
-                webBuilder.UseStartup<Startup>()
-                    .ConfigureKestrel(options =>
-                    {
-                        options.ConfigureHttpsDefaults(options =>
-                        {
-                            options.ServerCertificate = DevelopmentModeCertificateHelper.Certificate;
-                            options.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
-                        });
-                    });
+                options.AllowedCertificateTypes = CertificateTypes.Chained;
+                options.RevocationMode = X509RevocationMode.NoCheck;
+
+                options.Events = new CertificateAuthenticationEvents
+                {
+                    OnCertificateValidated = DevelopmentModeCertificateHelper.Validate
+                };
             });
-}
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseRouting();
+
+app.UseCertificateForwarding();
+app.UseAuthentication();
+app.MapGrpcService<FullStockTickerService>();
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+});
+
+app.Run();
