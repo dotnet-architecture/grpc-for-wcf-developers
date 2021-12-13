34bcead758fcc7094d0faa96b44c3e87c0c62928
Vaishali Raval <Vaishali.Raval@neudesic.com>
2021-12-13 17:43:05 +0530

    Deleted Startup and updated program.cs for  Kubernetes StockWeb

--- KubernetesSample/src/StockWeb/Program.cs
+++ KubernetesSample/src/StockWeb/Program.cs
@@ -1,16 +1,36 @@
 
-namespace StockWeb;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+var builder = WebApplication.CreateBuilder(args);
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-}
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+
+// Register the repository class as a scoped service (instance per request)
+
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<StockDataSettings>(builder.Configuration.GetSection("StockData"));
+
+builder.Services.AddGrpcClient<Stocks.StocksClient>((provider, options) =>
+{
+    var settings = provider.GetRequiredService<IOptionsMonitor<StockDataSettings>>();
+    options.Address = new Uri(settings.CurrentValue.Address);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
