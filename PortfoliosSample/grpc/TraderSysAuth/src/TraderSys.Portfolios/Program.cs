3e0ad9cf540886be1bc877bc4d29bdf7a3087bc2
Vaishali Raval <Vaishali.Raval@neudesic.com>
2021-12-13 17:30:00 +0530

    Deleted Startup and updated program.cs for TraderSysAuth

--- PortfoliosSample/grpc/TraderSysAuth/src/TraderSys.Portfolios/Program.cs
+++ PortfoliosSample/grpc/TraderSysAuth/src/TraderSys.Portfolios/Program.cs
@@ -1,22 +1,50 @@
 
-namespace TraderSys.Portfolios;
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+using TraderSys.Portfolios;
 
-    // Additional configuration is required to successfully run gRPC on macOS.
-    // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+
+// Register the repository class as a scoped service (instance per request)
+builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
+builder.Services.AddSingleton<IUserLookup, UserLookup>();
+
+builder.Services.AddGrpc();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+    {
+        policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+        policy.RequireClaim(ClaimTypes.Name);
+    });
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
             {
-                webBuilder.UseStartup<Startup>();
-                //                        .ConfigureKestrel(kestrelOptions =>
-                //                        {
-                //                            kestrelOptions.ListenLocalhost(5001, options => { options.Protocols = HttpProtocols.Http1AndHttp2; });
-                //                        });
-            });
-}
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateActor = false,
+                ValidateLifetime = true,
+                IssuerSigningKey = JwtHelper.SecurityKey
+            };
+    });
+
+var app = builder.Build();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+// Configure the HTTP request pipeline.
+app.MapGrpcService<PortfolioService>();
+app.MapGet("/generateJwtToken", async context =>
+{
+    await context.Response.WriteAsync(JwtHelper.GenerateJwtToken(context.Request.Query["name"]));
+});
+
+app.Run();
